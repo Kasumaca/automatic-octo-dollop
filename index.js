93289981b9eab5ab5fa76932ba3cf5a0bd3d0575
Kasumaca <58649780+Kasumaca@users.noreply.github.com>
2021-04-05 20:13:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ client.on('message', message => {
       if(message.content.toLowerCase() == 'kuro' && message.author.id !== '429095688228438017'){
         return message.channel.send(`Triệu tập ngài <@` + `429095688228438017` + `>`);
       }
-      if(message.content.toLowerCase().startsWith('luxenping')) {
+      if(message.content.toLowerCase().startsWith('miriping')) {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
         const m = await message.channel.send("Ping?");
