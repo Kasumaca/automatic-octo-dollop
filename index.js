db6778559efa9f38475982701bf9a475e43165cc
Kasumaca <58649780+Kasumaca@users.noreply.github.com>
2021-08-11 18:52:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ client.on("message", async message => {
 	    if(!message.guild.member(client.user).hasPermission('MANAGE_ROLES')) return;
 	    var colors = ['#8585ff','#fff681','#a073fd','#fd73b9'];
 	    for(let i = 0; i<= colors.length;i++){
-	    var role = message.guild.roles.find(role => role.name === "color4");
+	    var role = message.guild.roles.cache.find(role => role.name === "color4");
 	    setInterval(() => {
 		role.edit({
 		    color: colors[i]
