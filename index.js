03377bcf262bf96a83feead2724f6d9ae43c65a5
Kasumaca <58649780+Kasumaca@users.noreply.github.com>
2021-12-19 17:59:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ ftNIWP
 1Xhzay
 `);
       }
-	if(message.content.toLowerCase() == 'ember'){
+	if(message.content.toLowerCase() == 'miriember'){
           return message.channel.send(`Ghlf5y
 CJRny-
 yXw4m1
@@ -209,6 +209,652 @@ jUS79F
 31) Z6iZQ7
 32) QM7UGY`);
 	}
+	if(message.content.toLowerCase() == 'miridunes'){
+          return message.channel.send(`K9BFEp
+i9G1hh
+8EboDz
+Tx1NfQ
+7AQ2bI
+fxKLj9
+spjdre
+fI6ciS
+2ghHgb
+QVr68R
+6j4qiG
+SDRToC
+it09EW
+XUnm8n
+YADXIa
+up0xyJ 
+2mUzeU
+C6RE00
+kN4YI2 i or L
+4xTQPW
+1-f9bD
+kbO8jv
+M-dWJA
+1abTMa
+BIsGUK i or L
+Obk2fr 
+UP04BI i or L
+-ipp82
+qZtS9f
+Y6sRsW
+4B8_0t
+5SzeFY
+SszEg0
+XwV7xm
+7OYAi`);}
+	if(message.content.toLowerCase() == 'mirinimbus'){
+          return message.channel.send(`2)GcKqsG
+3)Lcbrjj
+4)eHIE79 (i or l)
+5)-wNuce
+6)sDm3SJ
+7)9RSkgw
+8)XB1im0
+9)U9KJxL
+10)4Kj-Iy
+11)pU9Vkc
+12)bJYKys
+13)6G4t-6
+14)zTwbtJ
+15)Eu9R_6
+16)_xBiCo
+17)XpMg8o
+18)qVTdhP
+19)x5gvUI (i or l)
+20)AFJ84W
+21)fb0JDg
+22)FMust7
+23)-VQy-p
+24)WR4d7x
+25)urX2rD
+26)uBAwdW
+27)gje4UM
+28)fTgPQG
+29)k3QbGc
+30)9YImuH (i or l)
+31)v-v-Aj
+32)yQobny
+33)DUAvPN
+34)NM_qTH
+35)9baNQZ
+36)gI7Nnv (i or l)
+37)0TSK2t (O or 0)
+38)C0mPKs (O or 0)
+39)8OqgeW (O or 0)
+40)R1xvyC
+41)I_N3UG (i or l)
+42)AkGFpN
+43)AWRuCJ
+44)JOtklM (i or l)
+45)_8UGei
+46)ssOQq6
+47)gcYcP2
+48)KbIzPZ (i or l)
+49)K2zyUV
+50)K4QiaT
+51)-M_va4
+52)9Kb5wM
+53)Q_aCLD
+54)EMYZ6x
+55)sg04vb (O or 0)
+56)pHmpQ2
+57)mLgmYE
+58)oKL2Si
+59)Y9wA3x
+60)xOFOiZ
+61)jzJcw9
+62)VI-d7l (i or l)
+63)0WfhIJ (i or l)
+64)8UZSyG
+65)K6L3us
+66)P_r9zF
+67)0nX3P3 (O or 0)
+68)_y1LwB
+69)sN1kVD
+70)pqwsiP
+71)bUqPot
+72)ERGkE1
+73)nzfqjH
+74)arvLxY
+75)88SquL
+76)Jl6srs (i or l)
+77)_wxVtz
+78)Sxy7X4
+79)z2RWHf
+80)IzYLW5 (i or l)
+81)S1OokY
+82)D2L5RV
+83)QDcs9G
+84)YhQl9 (i or l)
+85)-IVJB (i or l)
+86)KBd34X
+87)oe0SRf (O of 0)
+88)OBRtNZ
+89)4S7o3Y
+90)fRijNB
+91)O7s061
+92)sn66Qs
+93)f3gnXO
+94)_IwdTr (i or l)
+95)F_euOc
+96)nwTieM
+97)Dvgfpq
+98)uXQihc
+99)JfAusH
+100)m3qHI4 (i or l)`);}
+	if(message.content.toLowerCase() == 'miriwar'){
+          return message.channel.send(`1BFze5
+25pT2L
+2yqicv
+3C_0Qu
+-4mycE
+6-g1ej
+7Qs27m
+8GpNKH
+8LApEL
+adzBVR
+avXodn
+BkSTEN
+c-KzVK
+d_Z92O
+dCkx7C
+DPyLHT
+dttQxB
+EpefEq
+eWWqY9
+feub3n
+g4TdUa
+Hd67rr
+iQqtG6
+JC_QNh
+JcuP0p
+JQCw1U
+kRVY6O
+M3NMer
+MEwUFM
+mNLLzt
+MxW_Ue
+nqVJDE
+PcMCED
+pgFPqY
+RbH1SI
+RfuWaj
+rpbRMn
+sBKQMV
+SdUXCh
+S-frMS
+SNRTm4
+TcCnZ9
+Tcp7JK
+uUT_br
+v1eQNa
+V7pKDX
+VegfVx
+VR-sWX
+Vs1Bi-
+vsGhkE
+wFxr45
+WulgQQ
+xnejBK
+y8mMoB
+yBTzSq
+Yu7t-U
+yYRgjp
+Z2o3oE
+ZeQUVL
+zrA8gY`);}
+	if(message.content.toLowerCase() == 'miriobelisk'){
+          return message.channel.send(`9FLsXv
+9qDYIw
+9UQy3j
+AB7FiP
+ahNKzW
+aHOoBk
+B2LOMB
+BGjqnr
+dfLXmo
+dzuHJH
+YCWCKq
+YHPMt6
+yusZp4
+Z6DKqb
+2QLKJM
+2RAchb
+2X0dpj
+3D9E_U
+3RfgxS
+3UKFJ4
+4Akvn2
+7oC-He
+8hqjWO
+9ae9ra
+zAjXSn
+ZrTDOz
+E0Xgdq
+eb0Ec8
+EeCmWi
+fVRtOq
+fXi1h1
+GtZN40
+H2-a28
+HJcnLy
+HRDox5
+JYKuIK
+K3TtzE
+KhPK0U
+Kuh1H6
+kuKLv0
+Kya-NL
+m8Vxcg
+maiboK
+MAiHFD
+miPpJr
+mqeRUI
+mrmXGS
+msKaY3
+mw3qEF
+nSw_QA
+oi4Dbc
+pBA7Uz
+Pd6ZsH
+PiWPwp
+QIjafV
+QUOnXE
+r92PyJ
+rL46J-
+RXdQ7M
+S1p32U
+SfhiuM
+SflGW1
+SjDaMf
+T6DWj8
+TCEg7f
+tjWENr
+tyJq2t
+UKvX7q
+uNYdua
+v2HnyC
+VM2XQV
+VvHb5B
+vXnEWS
+wphj_x
+XbTF7h`);}
+	if(message.content.toLowerCase() == 'mirihaze'){
+          return message.channel.send(`Y1TA3i
+mjmSgZ
+LVFJJ3
+aZbEfQ
+YGC5JO
+Xs3zph
+mhLSN2
+FVnJBP
+VKrCqE
+md968J
+kTRGxF
+3mtFJr
+wkG5J0
+k7zvWY
+tYO9r9
+paT6BH
+VZGys6
+TNgFX5
+8X89GZ
+TkWWzW
+rMCxM1
+jsee7g
+BJoyow
+wa_eWb
+Pgg5bo
+_ZnC6M
+pk0XPb
+ATmccf
+7s26yN
+B8xJ5x
+k53Vi
+sG9OYx
+e9sFDO
+uLDetw
+EkUUbB
+_cmaW6
+fbnWzR
+dZcX2o
+2nSarb
+d-4kG9
+BRDto9
+kaPKB3
+btSpNE
+Zd8BTH
+8eyuSN
+Ha6eOT
+EENrUp
+gqNCH0
+EDOMQH
+9V4QNl
+J5rb8d
+Rw62v
+P3_3tR
+Y1TA3i
+Xs3zph
+B8xJ5x
+k53Vi
+sG9OYx
+e9sFDO
+btSpNE
+8eyuSN
+gqNCH0
+EDOMQH
+9V4QNl
+J5rb8d
+Rw62v
+gFzkM0
+8FDTck`);}
+	if(message.content.toLowerCase() == 'miritempest'){
+          return message.channel.send(`GXPSIR
+0qTMob
+m5SOxS
+qaHN3I
+J9MA16
+Fm17bC
+c82zhH
+BtbQKO
+bJHokK
+A0oE8Q
+mQjv7_
+APwkMy
+3lpd6u
+rOFDhE
+tYkDLR
+ZPXGUi
+1NulEe
+-TsdLs
+abzs9S
+Wqm5ss
+mV3kA7
+IozwaC
+6YUjHl
+5JVhKu
+DK8iOh
+9ZKpjJ
+xfcqR1
+AHgKYA
+db4Ghk
+doMwa7
+EEPzI-
+GMqiS3
+kLGyQc
+wQ99BY
+3SftE0
+S9xSt4
+FQnsJJ
+rVnCG3
+09-Xu9
+Dz8bqC
+522B-6
+4AUEPj
+ouDCuk
+jS7En
+3VGEBg
+Lz8FET
+Q2Wx9C
+YKaaS
+F3OELg
+eF_9qZ
+0aQTFt
+P5rUPZ
+QSTYOf
+G-4AQJ
+2UZCY4
+fri1K-
+mnHrJ0
+q1ehQS
+BmuPMt
+a1V8Lo
+mNfOlw
+1Q371v
+0Rrwfp
+m4ebgM
+`);}
+	if(message.content.toLowerCase() == 'miritraining'){
+          return message.channel.send(`wDTZy5
+rZQ9OL
+j-VOaw
+TuZHY2
+IHFiUm
+m-E3tV
+w0rZG1
+KcxLG
+IfPmNI
+tNf7pp
+Jqgrcs
+1SOdmO
+uEgyyb
+GGdMRx
+5Jb6_W
+2Tm-wH
+WSz2Ba
+PY4yx9
+1)Jgvoy2
+t_eA-G
+r_veyi
+0HL6PW
+wUo44s
+Ain7EW
+BAYp0s or BAYpOs
+LPXzsK
+fw4ZFz
+10)g8QPTE
+vpEi-5
+jUZdcY
+IyDyFV or lyDyFV
+nL_k-E
+sZ7S4K
+xMeWXM
+
+atYquM
+j58zIn or j58zln
+WwpNHp
+20)pS-Wkh
+WKK6uv
+Qr3GDm
+_Oikhj
+xn6J6m
+yKoBkp
+7CEM8-
+id7Dcw
+BmN31a
+MiSLm
+30)szHqkN
+15z-zA
+9XXJqz
+E41StY
+j2AinA
+3Vcq0t
+Hjryav
+T86inG
+sTn06Q
+34Fj0I or 34Fj0l
+40)sDTRYx
+OToSX3
+R47obo
+dsY1NB
+Qk-ikk
+Mtmtf3
+auhpf2
+jX2Tt9
+PmPGQT
+TUUF58
+50)dpwQFv
+oIXEoC or olXEoC
+TzqdHw
+HJJWZY
+P6q9UE
+y09eiQ
+yo0jBP
+pW8FAt
+ajDDO
+q0hZHm
+60)SkbWEc
+F2XxHM
+ti3TkD
+9bXwWt
+F6fVrS
+n9vvMZ
+iUkF4Q
+pnIdeo or pnldeo
+TDMSa2
+GK7S4Z
+70)cxjXM-
+GTG13i
+XF2ni8
+fzRmww
+rbui-I or rbui-l
+8iqt-M
+uTBPFZ
+0ULiyd
+jhLPZe
+kNuugv
+80)6m6rY1
+Z0Oyn4
+hamodG
+s0RvVb
+k5EfRj or kSEfRj
+O2aEcw
+AoOI-b or AoOl-b
+Xdzdzy
+VRGL7j
+PB7is-
+90)IQbGNQ or lQbGNQ
+TewYM9
+WehYLx
+XwLFkA
+1z8bNX
+xxlgeM or xxIgeM
+94dHP5 or 94dHPS
+u7oWxb
+8SJaZ8
+90DibT
+FbSsfQ
+LC9MzH
+100)56I97B or 56l97B`);}
+	if(message.content.toLowerCase() == 'miriforest'){
+          return message.channel.send(`oTNmov
+Pg7OJ1
+c34a5l
+eSx7Bq
+kHotIr
+j3pfj
+RY6dzS
+KkS7Xo
+CrLuTU
+QUMp-q
+TN1qmh
+yAw26R
+ar5Nnr
+DG2hfJ
+nVUcVd
+rQAjrb
+2eTvXy
+3BbkGL
+tEDIPc or tEDlPc
+LuOBNm
+I3YaH4
+Li569F
+bL1-C2
+85LbvI
+jFRnbY
+SAHzL1
+S4dSCt
+o0o1XD
+EaMtjZ
+o036Sy
+JubPfT
+zz-iCa
+F27fW1
+BX1ikD
+FYL_GN
+gnRrGo 
+caPPDR
+PVpaGV
+d_mRqn
+j7dsFc
+y0dxLR
+7v25Sw
+WH7qTn
+PLFMI1 i or L
+1zrsE2
+2bflbr or 2bfIbr
+KX7I_G  or KX7l_G 
+ONZryZ
+7unMdG
+0K7WHy
+zERGv5
+aMzXL2
+a56-Bs
+o996qx
+0mcG0p
+U328t1
+LfoJLf 
+Zz2Qcz
+GLF7Rc
+CET7_A
+BYSbyn
+EgNR1j
+I-EVt1 i or L
+2g8_bU
+TiJVpb 
+kk-YC7
+0iTJiz
+A2gA3U
+2g8_bU
+_xZvsJ
+QHIPEO or QHlPEO
+Rj5WKC
+qHKYRF
+5XpORC
+SuuG2y 
+wne0ro
+8AJHwl or 8AJHwI
+mJxGb2
+_721_c
+QsFuXN
+YFkTdw
+R0MNoK
+X0z-s4
+hCkwoN
+tvhm_6 
+vg_URs
+PmIqJb or PmlqJb
+eWhbid
+0yfnFI or 0yfnFl
+FBTp0z
+22PtCt
+_D4tTV
+6QiN5K
+SzmTcx
+5bfX9p 
+XHwFuN
+NuLxMG
+gTWhds
+tFkJmA
+TJX6_F
+zFUr3
+hU-5pm
+OVTHnn
+X-5IYT or X-5lYT
+HUqpZX 
+-TFxeU
+1uO9-k
+4DzZdT
+L5Zot
+YWcqXs
+3SU6Ng
+TOT1W2
+D2S1s-
+P_91
+Iec28 or l3c38`);}
       if(message.content.toLowerCase().startsWith('miriping')) {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
